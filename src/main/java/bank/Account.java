773af999dd46bbb2bf57573311a888b86ec3ecdb
JonLee33 <lee.jonathan.p@gmail.com>
2024-04-29 16:55:46 +0000

    create account class

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,39 @@
+package bank;
+
+import java.math.BigDecimal;
+
+public class Account {
+  private int id;
+  private String type;
+  private BigDecimal balance;
+
+  public Account(int id, String type, BigDecimal balance) {
+    setId(id);
+    setType(type);
+    setBalance(balance);
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public BigDecimal getBalance() {
+    return this.balance;
+  }
+
+  public void setBalance(BigDecimal balance) {
+    this.balance = balance;
+  }
+}
